1e7672c3a24fd6ecc66b03839e0fa6fc9e94b980
henrydrayton <henrydrayton0@gmail.com>
2021-04-06 11:36:42 +1000

    update timer code

--- T1_A3/game.rb
+++ T1_A3/game.rb
@@ -1,4 +1,9 @@
-require 'tty-font'
+require "./models/User"
 
-font = TTY::Font.new(:doom)
-puts font.write("THE UNDERDOG")
+RSpec.describe "Trivial Testing" do
+    it "should be able to add" do
+        actual_result = 1+1
+        expected_result = 2
+        expect(actual_result).to eq expected_result
+    end
+end
--- T1_A3/timer.rb
+++ T1_A3/timer.rb
@@ -1,9 +1,41 @@
-class Countdown
-require 'timers'
+# char.ord
 
-timers = Timers::Group.new
+require "timeout"
 
-every_five_seconds = timers.every(5) { puts "Another 5 seconds" }
+questions = [
+  "What are the following datatypes in Ruby?",
+  "What will any variable evaluate to?",
+  "Ruby is a case-sensitive language."
+]
 
-loop { timers.wait }
+# questions = [
+#   ["test", [ "option 1", "option 2", "option 3" ], 2 ],
+# ]
+
+answers = questions.map do |question|
+  puts question
+
+  answer = nil
+  begin
+    Timeout::timeout 5 do
+      answer = gets.chomp
+    end
+  rescue Timeout::Error => e
+    answer = nil
+  ensure
+    answer
+  end
 end
+
+p answers
+
+# puts "How are you?"
+# begin
+#   Timeout::timeout 5 do
+#     ans = gets.chomp
+#   end
+# rescue Timeout::Error => e
+#   p e
+#   ans = nil
+# end
+# puts (ans || "User did not respond")
