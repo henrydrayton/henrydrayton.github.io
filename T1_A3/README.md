be01f6b481f373be097fd9f9ce7c983a2b4cd8d9
henrydrayton <henrydrayton0@gmail.com>
2021-04-09 11:52:50 +1000

    add readme doc

---
+++ T1_A3/README.md
@@ -0,0 +1,11 @@
+#Software Development Plan
+
+### THE UNDERDOG 
+#####  Created by: Henry Drayton
+
+![Opeening Page](/docs/header.png "Opening Page").
+
+
+Most people dread their morning commute; at Coder Academy, students often dread their morning Kahoot. It seems as though there are a select few who take the podium spot each morning, leaving the rest of the class to question their adequacy. This terminal application is designed for students to test their knowledge of Ruby outside of class, preparing them for battle the next day. Accompanied by the monotonous Kahoot soundtrack, a fifteen-second time limit and inspirational quotes, this application turns students into an army that even William Wallace would be proud of. THE UNDERDOG is explicitly for Coder Academy students; however, it is also beneficial for anyone looking to refresh their Ruby general knowledge. Although the final application does not present the same graphical qualities as seen in Kahoot, it mentally prepares students and develops the quick hands required on game day. 
+
+When first starting the game, the title THE UNDERDOG  features a sizable graphical font display. The game works by first prompting the user with a question. Each question has up to four possible answers as well as a corresponding number to enter. The player answers each question by entering the relevant number and is only allowed one attempt per question. Their final score displays on completion of the quiz. Inspirational quotes also appear after each question to encourage users on their quest for knowledge and further mimic the Kahoot game.
--- T1_A3/spec/models/user_spec.rb
+++ T1_A3/spec/models/user_spec.rb
@@ -1,43 +1,87 @@
-require "./questions.rb"
 
-RSpec.describe User do
-    it { is_expected.to respond_to :name }
-    it { is_expected.to respond_to :save }
-end
 
-RSpec.describe "User" do
-    subject { User.new }
+require 'rspec'
+require '../T1_A3/src/THE_UNDERDOG'
 
-    it "should respond to name" do
-        expect(subject).to respond_to :name
+# -------- Testing the plane animation runs
+# RSpec.describe Answer do
+#     it 'should equal nil' do
+#         test_two = 
+#         # expect(Answer()).to eq nil
+#         expect(answer).to be_nil
+#     end
+# end
+RSpec.describe Answer do
+    it "has a balance of zero when first created" do
+      expect(Answer.new.balance).to eq(Score.new(0))
     end
-
-    it "should respond to save" do
-        expect(subject).to respond_to :save
-    end
-
-    describe "#save" do
-        it "should increase the length of Users by 1" do
-            expect { subject.save }.to change { User.all.length }.by 1
-        end
-    end
-
-    describe "#name" do
-        subject { (User.new "Adam").name }
-
-        it "should not be nil" do
-            expect(subject).not_to be_nil
-        end
-
-        it "should be assigned on initilisation if provided" do
-            expect(subject).to eq "Adam"
-        end
-    end
+  end
 
 
-end
+# RSpec.describe "Shared Variable" do
+#     subject { Answer.new }
+
+#     it "Should tell the user their score out of ten" do
+#         expect(answer).to respond_to :name
+#     end
+# end
 
 
-# change 
+    # it { is_expected.to respond_to :answers }
+    # it { is_expected.to respond_to :results }
+    # it { is_expected.not_to be_admin }
 
-# respond
+
+#     describe "#name" do
+#         subject { User.new.name }
+#         it { is_expected.not_to be_nil }
+#         it { is_expected.to match /anonymous/i }
+#     end
+# end
+
+# RSpec.describe "User" do
+#     subject { User.new }
+
+#     it "should respond to name" do
+#         expect(subject).to respond_to :name
+#     end
+
+#     it "should respond to save" do
+#         expect(subject).to respond_to :save
+#     end
+
+#     describe "#save" do
+#         it "should increase the length of Users by 1" do
+#             expect { subject.save }.to change { User.all.length }.by 1
+#         end
+#     end
+
+#     describe "#name" do
+#         subject { (User.new "Adam").name }
+
+#         it "should not be nil" do
+#             expect(subject).not_to be_nil
+#         end
+
+#         it "should be assigned on initilisation if provided" do
+#             expect(subject).to eq "Adam"
+#         end
+#     end
+
+
+# end
+
+# RSpec.describe "Trivial testing" do
+#     it "should able to add 1 and 1" do
+#         actual_result = 1+1
+#         expected_result = 2
+#         expect(actual_result).to eq expected_result
+#     end
+# end
+
+#     it "should not be equal to 3" do
+#         actual_result = 1+1
+#         expected_result = 3
+#         expect(actual_result).to be < expected_result
+#     end
+# end
